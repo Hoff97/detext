1fc2666c84b7d301c71436bd6adb133a5ef3b89b
Frithjof Winkelmann <frithjof97@web.de>
2019-10-25 18:48:33 +0200

    Setup first working inference

--- client/src/app/components/start/start.component.ts
+++ client/src/app/components/start/start.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import ndarray from 'ndarray';
-import { Tensor } from 'onnxjs';
 import { InferenceService } from 'src/app/services/inference.service';
 
 @Component({
@@ -16,20 +14,5 @@ export class StartComponent implements OnInit {
 
   async predictClass(image: ImageData) {
     await this.inferenceService.infer(image);
-  }
-
-  private preprocess(data, width, height) {
-    const dataFromImage = ndarray(new Float32Array(data), [width, height, 4]);
-
-    // Normalize 0-255 to (-1)-1
-    ndarray.ops.divseq(dataFromImage, 128.0);
-    ndarray.ops.subseq(dataFromImage, 1.0);
-
-    // mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
-
-
-    const tensor = new Tensor(new Float32Array(3 * width * height), 'float32', [1, 3, width, height]);
-    (tensor.data as Float32Array).set(dataFromImage.data);
-    return tensor;
   }
 }
--- client/src/app/services/inference.service.ts
+++ client/src/app/services/inference.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import ndarray from 'ndarray';
 import { InferenceSession, Tensor } from 'onnxjs';
 
 @Injectable({
@@ -13,16 +14,27 @@ export class InferenceService {
   }
 
   public async infer(image: ImageData) {
-    const inputs = [
-      new Tensor(new Float32Array([1.0, 2.0, 3.0, 4.0]), 'float32', [2, 2])
-    ];
-    const outputMap = await this.session.run(inputs);
+    const inputs = this.preprocess(image.data, image.width, image.height);
+    const outputMap = await this.session.run([inputs]);
     const outputTensor = outputMap.values().next().value;
+    console.log(outputTensor);
   }
 
   private async setupModel() {
     const url = 'assets/models/mobile_cnn.onnx';
     await this.session.loadModel(url);
-    console.log('yay');
+  }
+
+  private preprocess(data, width, height) {
+    const dataFromImage = ndarray(new Float32Array(data), [width, height, 4]);
+    const dataProcessed = ndarray(new Float32Array(width * height * 3), [1, 3, width, height]);
+
+    dataProcessed.pick(0, 0, null, null).set(dataFromImage.pick(null, null, 0));
+    dataProcessed.pick(0, 1, null, null).set(dataFromImage.pick(null, null, 1));
+    dataProcessed.pick(0, 2, null, null).set(dataFromImage.pick(null, null, 2));
+
+    const tensor = new Tensor(new Float32Array(3 * width * height), 'float32', [1, 3, width, height]);
+    (tensor.data as Float32Array).set(dataProcessed.data);
+    return tensor;
   }
 }
