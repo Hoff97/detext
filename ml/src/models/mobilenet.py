5ee98f862883c57fc21ae757723f9ac79132a64c
Frithjof Winkelmann <frithjof97@web.de>
2019-10-25 19:35:09 +0200

    Drop cropping and normalization in preprocessing

--- ml/src/models/mobilenet.py
+++ ml/src/models/mobilenet.py
@@ -33,8 +33,6 @@ class MobileNet(nn.Module):
             p.requires_grad = True
 
 preprocess = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    transforms.Resize(224),
+    transforms.ToTensor()
 ])
