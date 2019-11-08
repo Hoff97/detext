193b5628e717b630a5bcc889ae83cc9b136de2ee
Frithjof Winkelmann <frithjof97@web.de>
2019-11-08 21:05:12 +0100

    Add correct deployment script

---
+++ ci/deploy.sh
@@ -0,0 +1,9 @@
+docker push hoff97/detext-client
+docker push hoff97/detext-nginx
+docker push hoff97/detext-server
+
+docker-machine ls
+
+eval $(docker-machine env detext)
+docker-compose -f ci/docker-compose-deploy.yml down
+docker-compose -f ci/docker-compose-deploy.yml up -d
