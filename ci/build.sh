9e09f69a2357e3db8a91c4f3183a4f0b34ad34b6
Frithjof Winkelmann <frithjof97@web.de>
2019-11-08 19:30:17 +0100

    Add docker token and login

--- ci/build.sh
+++ ci/build.sh
@@ -1,14 +1,14 @@
 cd client
 
 docker build -t hoff97/detext-client .
-# docker push hoff97/detext-client
+docker push hoff97/detext-client
 
 cd ../server
 
 docker build -t hoff97/detext-server .
-# docker push hoff97/detext-server
+docker push hoff97/detext-server
 
 cd ../nginx
 
 docker build -t hoff97/detext-nginx .
-# docker push hoff97/detext-nginx
+docker push hoff97/detext-nginx
