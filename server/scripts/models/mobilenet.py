eae1d466d3421953f48533c02035dc74251a5753
Frithjof Winkelmann <frithjof97@web.de>
2019-11-30 13:02:47 +0100

    Add migration to update train image features

--- server/scripts/models/mobilenet.py
+++ server/scripts/models/mobilenet.py
@@ -10,17 +10,23 @@ class MobileNet(nn.Module):
     def __init__(self, features = 2, pretrained = True):
         super(MobileNet, self).__init__()
 
-        self.features = features
+        self.num_features = features
 
         self.mobilenet = models.mobilenet_v2(pretrained=pretrained)
         self.classifier = nn.Sequential(
             nn.Dropout(p=0.2, inplace=False),
-            nn.Linear(in_features=self.mobilenet.classifier[1].in_features, out_features=self.features, bias=True)
+            nn.Linear(in_features=self.mobilenet.classifier[1].in_features, out_features=self.num_features, bias=True)
         )
         self.mobilenet.classifier = self.classifier
 
     def forward(self, x):
         return self.mobilenet(x)
+
+    def features(self, x):
+        return self.mobilenet.features(x)
+
+    def linear(self, x):
+        return self.mobilenet.linear(x)
 
     def freeze(self):
         for p in self.mobilenet.parameters():
