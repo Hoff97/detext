a7ff2249fbe619704433ea6820a7f2137347ae6e
Frithjof Winkelmann <frithjof97@web.de>
2020-01-29 22:39:14 +0100

    Run tex2svg while testing backend

--- ci/test.sh
+++ ci/test.sh
@@ -1,3 +1,8 @@
 cd server
 pip install -r requirements.txt
+
+docker run -d --name tex2svg -p 0.0.0.0:9000:8000 hoff97/tex2svg
+
 coverage run --source='.' manage.py test detext.server detext/tests
+
+docker stop tex2svg
