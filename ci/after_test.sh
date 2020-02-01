5b37b9af5202c606821a15fa87c0f92fc8186609
Frithjof Winkelmann <frithjof97@web.de>
2020-02-01 14:05:47 +0100

    Add code style metric

--- ci/after_test.sh
+++ ci/after_test.sh
@@ -1,1 +1,17 @@
 bash <(curl -s https://codecov.io/bash)
+
+cd server
+COV=$(coverage report | grep TOTAL | grep -o '[^ ]*%')
+STYLE=$(flake8 ./ | wc -l)
+
+curl \
+  --header "Authorization: Token $SERIES_CI_TOKEN" \
+  --data value="$COV" \
+  --data sha="$(git rev-parse HEAD)" \
+  https://seriesci.com/api/repos/Hoff97/detext/cov/combined
+
+curl \
+  --header "Authorization: Token $SERIES_CI_TOKEN" \
+  --data value="$STYLE" \
+  --data sha="$(git rev-parse HEAD)" \
+  https://seriesci.com/api/repos/Hoff97/detext/style/combined
