6fb618fa7cd361dae16dcf20aac0f51d41dc8331
Frithjof Winkelmann <frithjof97@web.de>
2020-02-02 10:55:58 +0100

    Add client tests

--- ci/test.sh
+++ ci/test.sh
@@ -6,3 +6,7 @@ docker run -d --name tex2svg -p 0.0.0.0:9000:8000 hoff97/tex2svg
 coverage run --source='.' manage.py test detext.server detext/tests
 
 docker stop tex2svg
+
+cd ../client
+npm install
+npm run test-ci
